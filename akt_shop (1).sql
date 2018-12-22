28055fc2dcdd18e1c0a5fccb9275726874bf7581
dalimchy <dalimchyjony@gmail.com>
2018-12-22 20:05:15 +0600

    update

--- akt_shop (1).sql
+++ akt_shop (1).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 21, 2018 at 04:19 PM
+-- Generation Time: Dec 22, 2018 at 03:03 PM
 -- Server version: 10.1.37-MariaDB
--- PHP Version: 7.3.0
+-- PHP Version: 7.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -70,11 +70,7 @@ CREATE TABLE `tbl_category` (
 --
 
 INSERT INTO `tbl_category` (`category_id`, `category_name`, `category_image`, `category_description`, `publication_status`) VALUES
-(5, 'Men', 'upload/ca.jpg', 'Men', 1),
-(6, 'Women', 'upload/f3.jpg', 'Women', 1),
-(7, 'Boys', 'upload/PI8.jpg', 'Boys', 1),
-(8, 'Girls', 'upload/be2.jpg', 'Girls', 1),
-(9, 'Electronics', 'upload/cart.png', 'Electronics', 1);
+(1, 'TOP WEAR', 'upload/p30.jpg', 'TOP WEAR TOP WEARTOP WEARTOP WEARTOP WEARTOP WEARTOP WEARTOP WEARTOP WEARTOP WEARTOP WEAR', 1);
 
 -- --------------------------------------------------------
 
@@ -85,6 +81,7 @@ INSERT INTO `tbl_category` (`category_id`, `category_name`, `category_image`, `c
 CREATE TABLE `tbl_manufacture` (
   `manufacture_id` int(11) NOT NULL,
   `category_id` int(5) NOT NULL,
+  `manufacture_for` tinyint(2) NOT NULL DEFAULT '0',
   `manufacture_name` varchar(100) NOT NULL,
   `manufacture_image` varchar(250) NOT NULL,
   `manufacture_description` text NOT NULL,
@@ -95,16 +92,12 @@ CREATE TABLE `tbl_manufacture` (
 -- Dumping data for table `tbl_manufacture`
 --
 
-INSERT INTO `tbl_manufacture` (`manufacture_id`, `category_id`, `manufacture_name`, `manufacture_image`, `manufacture_description`, `publication_status`) VALUES
-(1, 5, 'Dresses', 'upload/pic3.jpg', 'Dresses', 1),
-(2, 5, 'Shoes ', 'upload/grid_pic3.jpg', 'Shoes ', 1),
-(3, 5, 'Jackets', 'upload/pic1.jpg', 'Jackets', 1),
-(4, 7, 'Sunglasses', 'upload/grid_pic2.jpg', 'Sunglasses', 1),
-(5, 6, 'Sport Wear', 'upload/pi13.jpg', 'Sport Wear', 1),
-(6, 6, 'Blazers', 'upload/f12.jpg', 'Blazers', 1),
-(7, 5, 'Shirts', 'upload/s31.jpg', 'Shirts', 1),
-(8, 9, 'Galaxy S', 'upload/bl21.jpg', 'Galaxy S', 1),
-(9, 5, 'Galaxy Note', 'upload/f11.jpg', 'Galaxy Note', 1);
+INSERT INTO `tbl_manufacture` (`manufacture_id`, `category_id`, `manufacture_for`, `manufacture_name`, `manufacture_image`, `manufacture_description`, `publication_status`) VALUES
+(1, 1, 1, 'Men - T-Shirte', 'upload/p24.jpg', 'T-Shirts  T-Shirts  T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts T-Shirts ', 1),
+(2, 1, 2, 'Women - Casual Shirts', 'upload/p25.jpg', 'Women - Casual ShirtsWomen - Casual ShirtsWomen - Casual ShirtsWomen - Casual ShirtsWomen - Casual ShirtsWomen - Casual ShirtsWomen - Casual ShirtsWomen - Casual ShirtsWomen - Casual ShirtsWomen - Casual ShirtsWomen - Casual ShirtsWomen - Casual ShirtsWomen - Casual ShirtsWomen - Casual ShirtsWomen - Casual ShirtsWomen - Casual ShirtsWomen - Casual ShirtsWomen - Casual Shirts', 1),
+(3, 1, 4, 'Girls - T-Shirts', 'upload/p13.jpg', 'Girls - T-ShirtsGirls - T-ShirtsGirls - T-ShirtsGirls - T-ShirtsGirls - T-ShirtsGirls - T-ShirtsGirls - T-ShirtsGirls - T-ShirtsGirls - T-ShirtsGirls - T-ShirtsGirls - T-ShirtsGirls - T-ShirtsGirls - T-ShirtsGirls - T-ShirtsGirls - T-ShirtsGirls - T-ShirtsGirls - T-ShirtsGirls - T-ShirtsGirls - T-ShirtsGirls - T-ShirtsGirls - T-Shirts', 1),
+(4, 1, 3, 'Boys T-shirt ', 'upload/p8.jpg', 'Boys T-shirt Boys T-shirt Boys T-shirt Boys T-shirt Boys T-shirt Boys T-shirt Boys T-shirt Boys T-shirt Boys T-shirt Boys T-shirt Boys T-shirt Boys T-shirt Boys T-shirt Boys T-shirt Boys T-shirt Boys T-shirt Boys T-shirt Boys T-shirt Boys T-shirt Boys T-shirt Boys T-shirt Boys T-shirt Boys T-shirt Boys T-shirt Boys T-shirt Boys T-shirt ', 1),
+(5, 1, 5, 'kids- T-Shirts', 'upload/p1.jpg', 'kids- T-Shirtskids- T-Shirtskids- T-Shirtskids- T-Shirtskids- T-Shirtskids- T-Shirtskids- T-Shirtskids- T-Shirts', 1);
 
 -- --------------------------------------------------------
 
@@ -140,12 +133,7 @@ CREATE TABLE `tbl_product` (
 --
 
 INSERT INTO `tbl_product` (`product_id`, `product_name`, `category_id`, `product_for`, `manufacture_id`, `product_short_description`, `product_long_description`, `product_price`, `product_new_price`, `product_quantity`, `product_image`, `product_img2`, `product_img3`, `product_img4`, `product_img5`, `product_tags`, `is_featured`, `publication_status`, `created_at`, `updated_at`) VALUES
-(1, 'Phillies Casual Long Sleeve Hoodie - Black', 5, 0, 1, 'Phillies Casual Long Sleeve Hoodie - Black', 'Phillies Casual Long Sleeve Hoodie - Black', 600.00, 550.00, 10, 'upload/202-500x500.png', NULL, NULL, NULL, NULL, NULL, 1, 1, '2018-12-21 13:41:55', NULL),
-(3, 'Black Phillies Hoodie Jacket For Men', 5, 0, 3, 'Black Phillies Hoodie Jacket For Men', 'Black Phillies Hoodie Jacket For Men', 1500.00, 1500.00, 4, 'upload/bl1.jpg', NULL, NULL, NULL, NULL, NULL, 0, 1, '2018-12-21 13:41:55', NULL),
-(4, 'test product2', 5, 0, 3, 'asdf', 'asd', 230.00, 100.00, 6, 'upload/PI83.jpg', NULL, NULL, NULL, NULL, NULL, 1, 1, '2018-12-21 13:41:55', NULL),
-(5, 'test product3', 6, 0, 2, 'asd', 'asdf', 500.00, 199.00, 5, 'upload/pic12.jpg', NULL, NULL, NULL, NULL, NULL, 1, 1, '2018-12-21 13:41:55', NULL),
-(6, 'test product4', 6, 0, 1, 'asd', 'asdf', 900.00, 100.00, 5, 'upload/pi1321.jpg', NULL, NULL, NULL, NULL, NULL, 1, 1, '2018-12-21 13:41:55', NULL),
-(7, 'test product5', 6, 0, 3, 'asd', 'asdf', 900.00, 300.00, 0, 'upload/pic13.jpg', NULL, NULL, NULL, NULL, NULL, 1, 1, '2018-12-21 13:41:55', NULL);
+(1, 'Aesthetic Outfitters Behemoth T-shirt (T-1217016)', 1, 1, 1, 'Aesthetic Outfitters Behemoth T-shirt T-1217016\r\nProduct Type: T-shirt.\r\nGender: Male\r\nFabric: Single jersey fine.\r\nCare: Hand Wash.\r\nColor: Black.\r\nWeight: 180 gm (Approx) Weights may vary according to fabric and sizes.', 'Aesthetic Outfitters Behemoth T-shirt T-1217016\r\nProduct Type: T-shirt.\r\nGender: Male\r\nFabric: Single jersey fine.\r\nCare: Hand Wash.\r\nColor: Black.\r\nWeight: 180 gm (Approx) Weights may vary according to fabric and sizes.', 900.00, 700.00, 50, 'upload/p4.jpg', NULL, NULL, NULL, NULL, NULL, 1, 1, '2018-12-22 13:21:12', NULL);
 
 --
 -- Indexes for dumped tables
@@ -189,19 +177,19 @@ ALTER TABLE `akt_users`
 -- AUTO_INCREMENT for table `tbl_category`
 --
 ALTER TABLE `tbl_category`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tbl_manufacture`
 --
 ALTER TABLE `tbl_manufacture`
-  MODIFY `manufacture_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `manufacture_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tbl_product`
 --
 ALTER TABLE `tbl_product`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
