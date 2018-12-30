4f0857c065f7768fa62b41de0e61bd61da01d3a5
AH <ahn.nayeem@gmail.com>
2018-12-30 18:53:29 +0600

    Change

--- application/config/routes.php
+++ application/config/routes.php
@@ -90,6 +90,8 @@ $route['update-product'] = 'supper_admin/update_product';
 
 $route['brands'] = 'supper_admin/add_brand';
 $route['save-brand'] = 'supper_admin/save_brand';
+// $route['edit-brand/(.+)'] = 'supper_admin/edit_brand/$1';
+$route['delete-brand/(.+)'] = 'supper_admin/delete_brand/$1';
 
 
 
--- application/views/admin/pages/add_manufacture.php
+++ application/views/admin/pages/add_manufacture.php
@@ -1,107 +1,110 @@
  <div class="row">
-                            <div class="col-lg-8">
-                                <!-- Bootstrap Forms Validation -->
-                                <!-- <h2 class="section-title">Add Category</h2> -->
-                                        <?php
-                                            $message = $this->session->userdata('message');
-                                            if ($message) { ?>
-												
-												<div class='alert alert-success alert-dismissable'>
-													<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-													<p><strong>Well done! </strong><?php echo $message; $this->session->unset_userdata('message'); ?></p>
-												</div>
-                                           <?php } ?>
-                                <div class="card">
-                                    <div class="card-header">
-                                        <!-- <h4>Validation</h4> -->
-                                        <ul class="card-actions">
-                                            <li>
-                                                <button type="button"><i class="ion-more"></i></button>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                    <div class="card-block">
-                                        <!-- jQuery Validation (.js-validation-bootstrap class is initialized in js/pages/base_forms_validation.js) -->
-                                        <!-- For more examples please check https://github.com/jzaefferer/jquery-validation -->
-                                        <form class="js-validation-bootstrap form-horizontal" action="<?php echo base_url();?>save-manufacture" enctype="multipart/form-data" method="post">
+    <div class="col-lg-8">
+        <!-- Bootstrap Forms Validation -->
+        <!-- <h2 class="section-title">Add Category</h2> -->
+        <?php
+            $message = $this->session->userdata('message');
+            if ($message) { ?>
+            
+            <div class='alert alert-success alert-dismissable'>
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                <p><strong>Well done! </strong><?php echo $message; $this->session->unset_userdata('message'); ?></p>
+            </div>
+        <?php } ?>
+        <div class="card">
+            <div class="card-header bg-teal bg-inverse">
+                <!-- <h4>Add Brand</h4> -->
+                <ul class="card-actions">
+                    <li>
+                        <button type="button" data-toggle="card-action" data-action="refresh_toggle" data-action-mode="demo"><i class="ion-refresh"></i></button>
+                    </li>
+                    <li>
+                        <button type="button" data-toggle="card-action" data-action="content_toggle"><i class="ion-chevron-down"></i></button>
+                    </li>
+                </ul>
+            </div>
+            <div class="card-block">
+                <!-- jQuery Validation (.js-validation-bootstrap class is initialized in js/pages/base_forms_validation.js) -->
+                <!-- For more examples please check https://github.com/jzaefferer/jquery-validation -->
+                <form class="js-validation-bootstrap form-horizontal" action="<?php echo base_url();?>save-manufacture" enctype="multipart/form-data" method="post">
 
-                                             <div class="form-group">
-                                                <label class="col-md-3 control-label" for="">Category Name <span class="text-orange">*</span></label>
-                                                <div class="col-md-8">
-                                                    <select class="form-control" id="" name="category_id" required="1">
-                                                        <option value="">Please select</option>
-                                                        <?php foreach ($publish_category_info as  $v_category) { ?>
-                                                        <option value="<?php echo $v_category->category_id?>"><?php echo $v_category->category_name?> </option>
-                                                        <?php } ?>
-                                                        
-                                                    </select>
-                                                </div>
-                                            </div>
-
-
-                                            <div class="form-group">
-                                                <label class="col-md-3 control-label" for="">Manufacture Name <span class="text-orange">*</span></label>
-                                                <div class="col-md-8">
-                                                    <input class="form-control" type="text" id=""  placeholder="Enter category name" name="manufacture_name" required />
-                                                </div>
-                                            </div>
-
-
-                                             <div class="form-group">
-                                                <label class="col-md-3 control-label" for="">Manufacture Image </label>
-                                                <div class="col-md-8">
-                                                    <input class="form-control" type="file" id="" name="manufacture_image" required="1">
-                                                </div>
-                                            </div>
-
-
-                                            
-                                            <div class="form-group">
-                                                <label class="col-md-3 control-label" for="">Manufacture Description <span class="text-orange">*</span></label>
-                                                <div class="col-md-8">
-                                                    <textarea class="form-control" id=""  rows="8" placeholder="Enter category description..." name="manufacture_description" required="1"></textarea>
-                                                </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <label class="col-md-3 control-label" for="">Manufacture For <span class="text-orange">*</span></label>
-                                                <div class="col-md-8">
-                                                    <select class="form-control" id="" name="manufacture_for" required="1">
-                                                        <option>Please select</option>
-                                                        <option value="1">Men</option>
-                                                        <option value="2">Women</option>
-                                                        <option value="3">Boys</option>
-                                                        <option value="4">Girls</option>
-                                                        <option value="5">Kids</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <label class="col-md-3 control-label" for="">Publication Status <span class="text-orange">*</span></label>
-                                                <div class="col-md-8">
-                                                    <select class="form-control" id="" name="publication_status" required="1">
-                    									<option value="">Please select</option>
-                    									<option value="1">Published </option>
-                                                        <option value="0">Unpublished </option>
-                    									
-                    								</select>
-                                                </div>
-                                            </div>
-                                         
-                                           
-                                            <div class="form-group m-b-0">
-                                                <div class="col-md-8 col-md-offset-4">
-                                                    <button class="btn btn-app" type="submit">Submit</button>
-                                                </div>
-                                            </div>
-                                        </form>
-                                    </div>
-                                    <!-- .card-block -->
-                                </div>
-                                <!-- .card -->
-                                <!-- Bootstrap Forms Validation -->
-                            </div>
-                            <!-- .col-lg-6 -->
-
-                   
-
+                        <div class="form-group">
+                        <label class="col-md-3 control-label" for="">Category Name <span class="text-orange">*</span></label>
+                        <div class="col-md-8">
+                            <select class="form-control" id="" name="category_id" required="1">
+                                <option value="">Please select</option>
+                                <?php foreach ($publish_category_info as  $v_category) { ?>
+                                <option value="<?php echo $v_category->category_id?>"><?php echo $v_category->category_name?> </option>
+                                <?php } ?>
+                                
+                            </select>
                         </div>
+                    </div>
+
+
+                    <div class="form-group">
+                        <label class="col-md-3 control-label" for="">Manufacture Name <span class="text-orange">*</span></label>
+                        <div class="col-md-8">
+                            <input class="form-control" type="text" id=""  placeholder="Enter category name" name="manufacture_name" required />
+                        </div>
+                    </div>
+
+
+                        <div class="form-group">
+                        <label class="col-md-3 control-label" for="">Manufacture Image </label>
+                        <div class="col-md-8">
+                            <input class="form-control" type="file" id="" name="manufacture_image" required="1">
+                        </div>
+                    </div>
+
+
+                    
+                    <div class="form-group">
+                        <label class="col-md-3 control-label" for="">Manufacture Description <span class="text-orange">*</span></label>
+                        <div class="col-md-8">
+                            <textarea class="form-control" id=""  rows="8" placeholder="Enter category description..." name="manufacture_description" required="1"></textarea>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label" for="">Manufacture For <span class="text-orange">*</span></label>
+                        <div class="col-md-8">
+                            <select class="form-control" id="" name="manufacture_for" required="1">
+                                <option>Please select</option>
+                                <option value="1">Men</option>
+                                <option value="2">Women</option>
+                                <option value="3">Boys</option>
+                                <option value="4">Girls</option>
+                                <option value="5">Kids</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label" for="">Publication Status <span class="text-orange">*</span></label>
+                        <div class="col-md-8">
+                            <select class="form-control" id="" name="publication_status" required="1">
+                                <option value="">Please select</option>
+                                <option value="1">Published </option>
+                                <option value="0">Unpublished </option>
+                                
+                            </select>
+                        </div>
+                    </div>
+                    
+                    
+                    <div class="form-group m-b-0">
+                        <div class="col-md-8 col-md-offset-3">
+                            <button class="col-md-12 btn btn-app-teal" type="submit">Add Subcategory</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <!-- .card-block -->
+        </div>
+        <!-- .card -->
+        <!-- Bootstrap Forms Validation -->
+    </div>
+    <!-- .col-lg-6 -->
+
+
+
+</div>
