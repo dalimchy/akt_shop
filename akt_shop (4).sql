e63c136cd2590d7ea2c2190fb865fc7cba65583a
dalimchy <dalimchyjony@gmail.com>
2019-01-20 23:50:52 +0600

    up

--- akt_shop (4).sql
+++ akt_shop (4).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 19, 2019 at 06:20 PM
+-- Generation Time: Jan 20, 2019 at 06:50 PM
 -- Server version: 10.1.37-MariaDB
--- PHP Version: 7.3.0
+-- PHP Version: 7.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -76,8 +76,7 @@ INSERT INTO `main_menu` (`id`, `item_name`, `fa_class`, `status`, `has_link`) VA
 (12, 'Electronics', 'fa fa-tablet', 1, ''),
 (13, 'Men', 'fa fa-male', 1, ''),
 (14, 'Women', 'fa fa-female', 1, ''),
-(15, 'Kids', 'fa fa-child', 1, ''),
-(16, 'Sports', 'fa fa-futbol-o', 1, '');
+(15, 'Kids', 'fa fa-child', 1, '');
 
 -- --------------------------------------------------------
 
@@ -97,15 +96,17 @@ CREATE TABLE `tbl_brand` (
 --
 
 INSERT INTO `tbl_brand` (`brand_id`, `brand_name`, `brand_logo`, `created_at`) VALUES
-(38, 'Gucci', 'upload/brand/1547797746_Gucci-logo.jpg', '2019-01-19 16:56:29'),
 (39, 'Levi\'s', 'upload/brand/1547797799_2000px-Levis_logo_svg.png', '2019-01-19 16:56:29'),
-(40, 'D&G', 'upload/brand/1547797848_254_480n.jpg', '2019-01-19 16:56:29'),
-(41, 'H&M', 'upload/brand/1547797880_HM-Share-Image.jpg', '2019-01-19 16:56:29'),
-(42, 'american eagle', 'upload/brand/1547797939_xHZIQLF.png', '2019-01-19 16:56:29'),
 (44, 'Lotto', 'upload/brand/1547798073_a7d5c2cbe8bdb83262f3babf9388d42c.jpeg', '2019-01-19 16:56:29'),
 (45, 'Armani', 'upload/brand/1547798114_emporio-armani-logo-vector-1040x737.png', '2019-01-19 16:56:29'),
-(46, '420_Brand', 'upload/brand/1547918118_45227946_1916214918463290_8577545737359851520_n.jpg', '2019-01-19 17:15:18'),
-(47, '420_Brand', '', '2019-01-19 17:19:46');
+(48, 'Apple', 'upload/brand/1548002176_7a645fe6ffb2a4475a607337a7b9f08a.jpg', '2019-01-20 16:36:16'),
+(49, 'Samsung', 'upload/brand/1548002208_Samsung-logo-2015-Nobg.png', '2019-01-20 16:36:48'),
+(50, 'MI', 'upload/brand/1548002242_Xiaomi_logo_Mi-700x700.png', '2019-01-20 16:37:22'),
+(51, 'Nokia', 'upload/brand/1548002304_Xiaomi_logo_Mi-700x700.png', '2019-01-20 16:38:24'),
+(52, 'HUAWEI', 'upload/brand/1548002359_Huawei-Logo.jpg', '2019-01-20 16:39:19'),
+(53, 'OPPO', 'upload/brand/1548002390_OPPO_Logo_wiki.png', '2019-01-20 16:39:50'),
+(54, 'ASUS', 'upload/brand/1548002750_1_NwfoiV9f96_MhpmJwdinPA.png', '2019-01-20 16:45:50'),
+(55, 'HP', 'upload/brand/1548002785_Colors-HP-Logo.jpg', '2019-01-20 16:46:25');
 
 -- --------------------------------------------------------
 
@@ -154,7 +155,10 @@ INSERT INTO `tbl_category` (`category_id`, `category_name`, `category_image`, `m
 (19, 'Traditional Wear', 'upload/category/p8122.jpg', 13, 0, 0, 'test', 1),
 (20, 'Women\'s Clothing', 'upload/category/p8123.jpg', 14, 0, 0, 'test', 1),
 (21, 'Baby & Kids Clothing', 'upload/category/b3e48d56fce0d45caab996badce96acf_jpg_340x340q901.jpg', 15, 0, 0, 'test', 1),
-(22, 'Exercise & Fitness', 'upload/category/p81231.jpg', 16, 0, 0, 'test', 1);
+(22, 'Exercise & Fitness', 'upload/category/p81231.jpg', 16, 0, 0, 'test', 1),
+(26, 'Baby & Kids Clothing', 'upload/category/Xiaomi_logo_Mi-700x7003.png', 15, 0, 0, 'asdf', 1),
+(27, 'Beauty & Grooming', 'upload/category/Xiaomi_logo_Mi-700x7004.png', 14, 0, 0, 'test', 1),
+(28, 'Women\'s Accessories', 'upload/category/Xiaomi_logo_Mi-700x7005.png', 14, 0, 0, 'test', 1);
 
 -- --------------------------------------------------------
 
@@ -201,12 +205,39 @@ CREATE TABLE `tbl_manufacture` (
 --
 
 INSERT INTO `tbl_manufacture` (`manufacture_id`, `category_id`, `manufacture_for`, `manufacture_name`, `manufacture_image`, `manufacture_description`, `publication_status`) VALUES
-(1, 11, 1, 'iPhone', 'upload/subcategory/p2412.jpg', 'test', 1),
-(2, 11, 1, 'Samsung', 'upload/subcategory/p24121.jpg', 'test', 1),
-(3, 12, 2, 'Samsung', 'upload/subcategory/p812.jpg', 'test', 1),
-(4, 14, 2, 'ASUS', 'upload/subcategory/p182.jpg', 'test', 1),
-(5, 18, 1, 'T-Shirts', 'upload/subcategory/p24122.jpg', 'test', 1),
-(6, 18, 2, 'Sweater', 'upload/subcategory/p1811.jpg', 'asd', 1);
+(7, 11, 0, 'iPhone', 'upload/subcategory/images.png', 'test', 1),
+(8, 11, 0, 'Samsung', 'upload/subcategory/nokia-phones-lead-05-1480939336.jpg', 'test', 1),
+(9, 11, 0, 'Huawei', 'upload/subcategory/Huawei-Logo.jpg', 'test', 1),
+(10, 11, 0, 'Nokia', 'upload/subcategory/nokia-phones-lead-05-14809393361.jpg', 'test', 1),
+(11, 12, 0, 'Samsung', 'upload/subcategory/Huawei-Logo1.jpg', 'test', 1),
+(12, 12, 0, 'MI', 'upload/subcategory/Xiaomi_logo_Mi-700x700.png', 'test', 1),
+(13, 11, 0, 'MI', 'upload/subcategory/Xiaomi_logo_Mi-700x7001.png', 'test', 1),
+(14, 13, 0, 'Flip Cover', 'upload/subcategory/Xiaomi_logo_Mi-700x7002.png', 'test', 1),
+(15, 13, 0, 'Back Cover', 'upload/subcategory/Xiaomi_logo_Mi-700x7003.png', 'test', 1),
+(16, 13, 0, 'Screen Protector', 'upload/subcategory/Xiaomi_logo_Mi-700x7004.png', 'test', 1),
+(17, 14, 0, 'ASUS', 'upload/subcategory/Huawei-Logo2.jpg', 'test', 1),
+(18, 14, 0, 'APPLE', 'upload/subcategory/7a645fe6ffb2a4475a607337a7b9f08a.jpg', 'test', 1),
+(19, 14, 0, 'HP', 'upload/subcategory/nokia-phones-lead-05-14809393362.jpg', 'test', 1),
+(20, 15, 0, 'ASUS', 'upload/subcategory/Xiaomi_logo_Mi-700x7005.png', 'test', 1),
+(21, 15, 0, 'MAC', 'upload/subcategory/7a645fe6ffb2a4475a607337a7b9f08a1.jpg', 'test', 1),
+(22, 16, 0, 'Printer', 'upload/subcategory/Xiaomi_logo_Mi-700x7006.png', 'test', 1),
+(23, 16, 0, 'Motherboard', 'upload/subcategory/Xiaomi_logo_Mi-700x7007.png', 'test', 1),
+(24, 16, 0, 'Monitor', 'upload/subcategory/Xiaomi_logo_Mi-700x7008.png', 'test', 1),
+(25, 17, 0, 'SONY', 'upload/subcategory/Xiaomi_logo_Mi-700x7009.png', 'test', 1),
+(26, 17, 0, 'Samsung', 'upload/subcategory/Xiaomi_logo_Mi-700x70010.png', 'test', 1),
+(27, 18, 0, 'T-Shirts', 'upload/subcategory/Xiaomi_logo_Mi-700x70011.png', 'test', 1),
+(28, 18, 0, 'Casual Shirts', 'upload/subcategory/Xiaomi_logo_Mi-700x70012.png', '', 1),
+(29, 18, 0, 'Suits', 'upload/subcategory/Xiaomi_logo_Mi-700x70013.png', '', 1),
+(30, 18, 0, 'Formal Shirts', 'upload/subcategory/Xiaomi_logo_Mi-700x70014.png', '', 1),
+(31, 18, 0, 'Hoodies', 'upload/subcategory/Xiaomi_logo_Mi-700x70015.png', '', 1),
+(32, 19, 0, 'Panjabi', 'upload/subcategory/Xiaomi_logo_Mi-700x70016.png', '', 1),
+(33, 19, 0, 'Pajama', 'upload/subcategory/Xiaomi_logo_Mi-700x70017.png', '', 1),
+(34, 20, 0, 'Sweater', 'upload/subcategory/Xiaomi_logo_Mi-700x70018.png', 't', 1),
+(35, 20, 0, 'Hoodies', 'upload/subcategory/Xiaomi_logo_Mi-700x70019.png', 'test', 1),
+(36, 20, 0, 'Coats & Jackets', 'upload/subcategory/Xiaomi_logo_Mi-700x70020.png', '', 1),
+(37, 21, 0, 'Baby Diapers', 'upload/subcategory/Xiaomi_logo_Mi-700x70021.png', '', 1),
+(38, 21, 0, 'Baby Wipes', 'upload/subcategory/Xiaomi_logo_Mi-700x70022.png', 'test', 1),
+(39, 21, 0, 'Baby Bath & Skin', 'upload/subcategory/Xiaomi_logo_Mi-700x70023.png', 'Baby Bath & Skin', 1);
 
 -- --------------------------------------------------------
 
@@ -262,9 +293,15 @@ CREATE TABLE `tbl_product` (
 --
 
 INSERT INTO `tbl_product` (`product_id`, `product_name`, `brand_id`, `product_model`, `pro_label`, `category_id`, `product_for`, `manufacture_id`, `product_short_description`, `product_long_description`, `product_price`, `product_new_price`, `product_quantity`, `product_image`, `product_img2`, `product_img3`, `product_img4`, `product_img5`, `product_tags`, `is_featured`, `publication_status`, `created_at`, `updated_at`) VALUES
-(1, 'Apple iPhone 6 32GB', 38, 'M205', NULL, 11, NULL, 1, 'Apple iPhone 6 32GB Mobile Phone\r\nOS: iOS 8', 'Apple iPhone 6 32GB Mobile Phone\r\nOS: iOS 8\r\nProcessor: Dual-Core 1.4 GHz Cyclone (ARM v8-based) Processor\r\nChipset: Apple A8\r\nRear Camera: 8 Megapixel Camera (3264 x 2448 pixels)\r\nFront Camera: 1.2 Megapixel \r\nDisplay: 4.7-Inch LED-backlit IPS LCD Multi-Touchscreen (750 x 1334 pixels)\r\nSensor: Fingerprint (front-mounted), accelerometer, gyro, proximity, compass, barometer\r\nBattery: Non-removable Li-Po 1810 mAh battery (6.9 Wh)\r\niPhone 6 Smartphone: OS\r\niOS is the operating system that powers every iPhone mobile. It lets you do things you simply can’t with other mobile phone operating system. That’s because it’s designed specifically for the hardware it runs on and vice versa. So your iPhone works like magic with your other Apple devices. And it works hand in hand with iCloud to keep photos, documents, and other stuff up to date on those smart devices. iOS also comes with an entire suite of beautifully designed apps. And it’s been built from the ground up with privacy and security in mind. iOS 8 using in this Apple iPhone.\r\n\r\niPhone 6 Mobile Phone: Storage; Camera; Battery\r\nIt comes with 1GB of RAM. The smartphone packs 32GB of internal storage cannot be expanded. With Apple A8 chipset and 1GB of RAM is able to provide smooth performance. As far as the phone cameras are concerned, the Apple iPhone 6 packs an 8-megapixel primary camera on the rear and a 1.2-megapixel front shooter for selfies. And the cameras are able to take brilliant photos. The primary camera also able to record 1080P videos. 1810mAh non-removable battery use in this iPhone 6. ', 29999.00, 28999.00, 500, 'upload/products/511216_apple_iphone_6_32gb_phone_-_gold_2_result.jpg', 'upload/products/download.jpeg', NULL, NULL, NULL, NULL, 0, 1, '2019-01-19 16:56:31', NULL),
-(2, 'Apple iPhone 6S Plus 32 GB', 38, 'M206', NULL, 11, NULL, 1, 'Apple iPhone 6 32GB Mobile Phone\r\nOS: iOS 8\r\nProcessor: Dual-Core 1.4 GHz Cyclone (ARM v8-based) Processor\r\nChipset: Apple A8\r\nRear Camera: 8 Megapixel Camera (3264 x 2448 pixels)\r\nFront Camera: 1.2 Megapixel \r\nDisplay: 4.7-Inch LED-backlit IPS LCD Multi-Touchscreen (750 x 1334 pixels)\r\nSensor: Fingerprint (front-mounted), accelerometer, gyro, proximity, compass, barometer\r\nBattery: Non-removable Li-Po 1810 mAh battery (6.9 Wh)', 'Apple iPhone 6 32GB Mobile Phone\r\nOS: iOS 8\r\nProcessor: Dual-Core 1.4 GHz Cyclone (ARM v8-based) Processor\r\nChipset: Apple A8\r\nRear Camera: 8 Megapixel Camera (3264 x 2448 pixels)\r\nFront Camera: 1.2 Megapixel \r\nDisplay: 4.7-Inch LED-backlit IPS LCD Multi-Touchscreen (750 x 1334 pixels)\r\nSensor: Fingerprint (front-mounted), accelerometer, gyro, proximity, compass, barometer\r\nBattery: Non-removable Li-Po 1810 mAh battery (6.9 Wh)\r\niPhone 6 Smartphone: OS\r\niOS is the operating system that powers every iPhone mobile. It lets you do things you simply can’t with other mobile phone operating system. That’s because it’s designed specifically for the hardware it runs on and vice versa. So your iPhone works like magic with your other Apple devices. And it works hand in hand with iCloud to keep photos, documents, and other stuff up to date on those smart devices. iOS also comes with an entire suite of beautifully designed apps. And it’s been built from the ground up with privacy and security in mind. iOS 8 using in this Apple iPhone.\r\n\r\niPhone 6 Mobile Phone: Storage; Camera; Battery\r\nIt comes with 1GB of RAM. The smartphone packs 32GB of internal storage cannot be expanded. With Apple A8 chipset and 1GB of RAM is able to provide smooth performance. As far as the phone cameras are concerned, the Apple iPhone 6 packs an 8-megapixel primary camera on the rear and a 1.2-megapixel front shooter for selfies. And the cameras are able to take brilliant photos. The primary camera also able to record 1080P videos. 1810mAh non-removable battery use in this iPhone 6. ', 29999.00, 0.00, 10, 'upload/products/iphone-6s.jpg', 'upload/products/iPhone_6_Back.jpg', NULL, NULL, NULL, NULL, 1, 1, '2019-01-19 16:56:31', NULL),
-(4, 'check', 45, 'X550LA', 'Hot', 14, NULL, 4, 'check', 'check', 45000.00, 0.00, 2, '', NULL, NULL, NULL, NULL, NULL, 1, 1, '2019-01-19 17:14:18', NULL);
+(5, 'Apple iPhone 6 32GB', 48, '1', NULL, 11, NULL, 7, 'Apple iPhone 6 32GB Mobile Phone\r\nOS: iOS 8\r\nProcessor: Dual-Core 1.4 GHz Cyclone (ARM v8-based) Processor\r\nChipset: Apple A8\r\nRear Camera: 8 Megapixel Camera (3264 x 2448 pixels)\r\nFront Camera: 1.2 Megapixel \r\nDisplay: 4.7-Inch LED-backlit IPS LCD Multi-Touchscreen (750 x 1334 pixels)\r\nSensor: Fingerprint (front-mounted), accelerometer, gyro, proximity, compass, barometer\r\nBattery: Non-removable Li-Po 1810 mAh battery (6.9 Wh)', 'Apple iPhone 6 32GB Mobile Phone\r\nOS: iOS 8\r\nProcessor: Dual-Core 1.4 GHz Cyclone (ARM v8-based) Processor\r\nChipset: Apple A8\r\nRear Camera: 8 Megapixel Camera (3264 x 2448 pixels)\r\nFront Camera: 1.2 Megapixel \r\nDisplay: 4.7-Inch LED-backlit IPS LCD Multi-Touchscreen (750 x 1334 pixels)\r\nSensor: Fingerprint (front-mounted), accelerometer, gyro, proximity, compass, barometer\r\nBattery: Non-removable Li-Po 1810 mAh battery (6.9 Wh)', 29999.00, 0.00, 50, '', 'upload/products/iphone-iphone6-colors.jpg', NULL, NULL, NULL, NULL, 0, 1, '2019-01-20 17:14:22', NULL),
+(6, 'Samsung Galaxy J7 Duo', 49, '2', NULL, 11, NULL, 8, 'test', 'Samsung Galaxy J7 Duo Mobile - Ram: 4GB I ROM: 32GB\r\nOperating System: Android Oreo 8.0\r\nProcessor: Octa-core 1.6 GHz\r\nDisplay Size: 5.5” HD Super AMOLED\r\nRear Camera: 13MP (F1.9)/ 5MP (1.9)\r\nFront Camera: 8MP (F1.9)\r\nMemory: 4 GB Ram + 32 GB ROM\r\nExpandalbe Memory: microSD up to 256 GB\r\nBattery: 3,000 mAh (Li-ion-Removable)\r\nSamsung Galaxy J7 Duo smartphone comes with the Dual Camera, So turn up the fun with the dual camera.', 24990.00, 0.00, 20, '', NULL, NULL, NULL, NULL, NULL, 1, 1, '2019-01-20 17:16:23', NULL),
+(7, 'Xiaomi Mi A1 4GB/32GB', 50, '3', 'New', 11, NULL, 13, 'test', 'Xiaomi Mi A1 Mobile Phone 4GB/32GB\r\nOperating System: Android 7.1.2 (Nougat), planned upgrade to Android 8.0 (Oreo)\r\nSIM: Hybrid Dual SIM (Nano-SIM, dual stand-by)\r\nProcessor: Octa-core 2.0 GHz Cortex-A53\r\nChipset: Qualcomm MSM8953 Snapdragon 625\r\nGPU: Adreno 506\r\nDisplay Size: 5.5 inch (diagonal) LTPS FHD display\r\nResolution: 1080 x 1920 pixels (~403 ppi pixel density)\r\nRear Camera: Dual 12 MP (26mm, f/2.2; 50mm, f/2.6), phase detection autofocus, 2x optical zoom, dual-LED (dual tone) flash\r\nFront Camera: 5 MP, 1080p\r\nVideo: 4K / 1080p / 720p video, 30 fps; 720p slow-mo video, 120 fps\r\nSensors: Fingerprint (rear-mounted), accelerometer, gyro, proximity, compass\r\nGPS Navigation: GPS; AGPS; GLONASS; BEIDOU\r\nConnectivity & Wireless: 3-choose-2; Nano SIM/MicroSD; Supports 802.11a/b/g/n/ac protocols; Supports 2.4/5G WiFi / WIFI Direct / WiFi Display; Supports Bluetooth 4.2/HID\r\n4G: Supports VoLTE/4G/3G/2G on compatible networks\r\nBattery: Non-removable Li-Ion 3080 mAh battery', 21990.00, 0.00, 20, 'upload/products/8d4f4a93-a6e3-4b32-b154-0956aea2ffbb.jpg', 'upload/products/71tJdEpRFUL__SX425_.jpg', NULL, NULL, NULL, NULL, 0, 1, '2019-01-20 17:18:27', NULL),
+(8, 'Nokia 6 2017', 51, 'M205', 'New', 11, NULL, 10, 'test', 'test', 15500.00, 0.00, 4, '', NULL, NULL, NULL, NULL, NULL, 1, 1, '2019-01-20 17:20:14', NULL),
+(9, 'Samsung Guru Music 2', 49, 'd', 'Hot', 12, NULL, 11, 'test', 'demo test', 5000.00, 3000.00, 50, '', NULL, NULL, NULL, NULL, NULL, 1, 1, '2019-01-20 17:22:06', NULL),
+(10, 'Asus E203MAH PQC N5000 11.6” 4GB/500GB Laptop', 54, 'd', 'New', 14, NULL, 17, 'test', 'test', 28840.00, 0.00, 50, '', NULL, NULL, NULL, NULL, NULL, 0, 1, '2019-01-20 17:23:41', NULL),
+(11, 'Apple MacBook Air 2018 Core i5 13.3\" 8GB/128GB SSD with Retina Display (MREE2LL/A)', 48, 'a', 'Hot', 14, NULL, 18, 'test', 'test', 113300.00, 103300.00, 10, '', NULL, NULL, NULL, NULL, NULL, 1, 1, '2019-01-20 17:26:28', NULL),
+(12, 'Adidas Chelsea EU Training Mens Jersey', 44, 'df', 'New', 18, NULL, 27, 'test', 'test', 5000.00, 3000.00, 50, 'upload/products/chelsea-1.jpg', NULL, NULL, NULL, NULL, NULL, 0, 1, '2019-01-20 17:27:35', NULL),
+(13, 'Official Star Wars Mens T Shirt (Stormtroo Power)', 44, 'M206', 'New', 18, NULL, 27, 'test', 'test', 3000.00, 1500.00, 10, '', NULL, NULL, NULL, NULL, NULL, 0, 1, '2019-01-20 17:29:01', NULL);
 
 -- --------------------------------------------------------
 
@@ -386,7 +423,7 @@ ALTER TABLE `main_menu`
 -- AUTO_INCREMENT for table `tbl_brand`
 --
 ALTER TABLE `tbl_brand`
-  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
 -- AUTO_INCREMENT for table `tbl_cart`
@@ -398,7 +435,7 @@ ALTER TABLE `tbl_cart`
 -- AUTO_INCREMENT for table `tbl_category`
 --
 ALTER TABLE `tbl_category`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `tbl_customers`
@@ -410,7 +447,7 @@ ALTER TABLE `tbl_customers`
 -- AUTO_INCREMENT for table `tbl_manufacture`
 --
 ALTER TABLE `tbl_manufacture`
-  MODIFY `manufacture_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `manufacture_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT for table `tbl_orders`
@@ -422,7 +459,7 @@ ALTER TABLE `tbl_orders`
 -- AUTO_INCREMENT for table `tbl_product`
 --
 ALTER TABLE `tbl_product`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `tbl_review`
