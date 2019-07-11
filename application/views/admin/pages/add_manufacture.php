d37140179261a1e72476104c385c40c49bc26aae
AH Nayeem <ahn.nayeem@gmail.com>
2019-07-11 20:49:19 +0600

    up

--- application/views/admin/pages/add_manufacture.php
+++ application/views/admin/pages/add_manufacture.php
@@ -56,19 +56,7 @@
                             <textarea class="form-control" id=""  rows="6" placeholder="Enter category description..." name="manufacture_description"></textarea>
                         </div>
                     </div>
-                    <!-- <div class="form-group"> -->
-                        <!-- <label class="col-md-3 control-label" for="">Subcategory For <span class="text-orange">*</span></label> -->
-                        <!-- <div class="col-md-8"> -->
-                            <!-- <select class="form-control" id="" name="manufacture_for" required="1"> -->
-                                <!-- <option>Please select</option> -->
-                                <!-- <option value="1">Men</option> -->
-                                <!-- <option value="2">Women</option> -->
-                                <!-- <option value="3">Boys</option> -->
-                                <!-- <option value="4">Girls</option> -->
-                                <!-- <option value="5">Kids</option> -->
-                            <!-- </select> -->
-                        <!-- </div> -->
-                    <!-- </div> -->
+
                     <div class="form-group">
                         <label class="col-md-3 control-label" for="">Publication Status <span class="text-orange">*</span></label>
                         <div class="col-md-8">
